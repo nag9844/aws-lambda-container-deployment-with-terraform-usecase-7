b5a38b7b4ec959eae27ba48d702ef967ba9a630d
nag9844 <vnagaraj984@gmail.com>
2025-06-08 00:52:00 +0530

    update config

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -18,7 +18,7 @@ output "registry_id" {
   value       = aws_ecr_repository.main.registry_id
 }
 
-output "repository_url" {
-  description = "URL of the repository"
-  value       = aws_ecr_repository.main.repository_url
-}
+# output "repository_url" {
+#   description = "URL of the repository"
+#   value       = aws_ecr_repository.main.repository_url
+# }
