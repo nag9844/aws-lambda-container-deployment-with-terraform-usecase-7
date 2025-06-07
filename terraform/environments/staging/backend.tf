1bc1bf0c99b453de91707b254e595c3f1d8a4594
nag9844 <vnagaraj984@gmail.com>
2025-06-07 22:15:07 +0530

    update config

--- terraform/environments/staging/backend.tf
+++ terraform/environments/staging/backend.tf
@@ -1,5 +1,5 @@
-bucket = usecases-terraform-state-bucket
-key = usecase6/staging/terraform.tfstate
-region = ap-south-1
+bucket = "usecases-terraform-state-bucket"
+key = "usecase6/staging/terraform.tfstate"
+region = "ap-south-1"
 encrypt = true
 use_lockfile = true
