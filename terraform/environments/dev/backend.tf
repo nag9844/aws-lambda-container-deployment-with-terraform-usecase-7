a031f8587f963035d2db447f273fd2240ae6811e
nag9844 <vnagaraj984@gmail.com>
2025-06-07 22:11:24 +0530

    update config

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,5 @@
-terraform {
-  backend "s3" {
-	bucket         = "usecases-terraform-state-bucket"
-	key            = "usecase6/dev/terraform.tfstate"
-	region         = "ap-south-1"
-	encrypt        = true
-	use_lockfile   = true
-  }
-}
+bucket = "usecases-terraform-state-bucket"
+key = "usecase6/dev/terraform.tfstate"
+region = "ap-south-1"
+encrypt = true
+use_lockfile = true
