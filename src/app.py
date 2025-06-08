32093cd92816b345c3bc5c06fa59644a00d4984f
nag9844 <vnagaraj984@gmail.com>
2025-06-08 21:48:37 +0530

    update config

--- src/app.py
+++ src/app.py
@@ -28,5 +28,5 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type'
         },
-        'body': 'Hello World! This is a Lambda function running in a container.'
+        'body': 'Hello World!'
     }
