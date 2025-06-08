dc1282e0076c689e97345bee80dd4e71f9117ec5
nag9844 <vnagaraj984@gmail.com>
2025-06-08 13:40:13 +0530

    update config

--- src/app.py
+++ src/app.py
@@ -12,5 +12,5 @@ def handler(event, context):
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type'
         },
-        'body': 'Hello World'
+        'body': 'Hello World!'
     }
